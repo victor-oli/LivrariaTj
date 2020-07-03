bfe75ca14be04f9bdc922a36928df716c4a85704
Victor Oliveira <jvespinosa@stone.com.br>
2020-07-03 17:46:36 -0300

    implement assunto repository

---
+++ Tj.Livraria/Tj.Livraria.Infra/Repositories/AssuntoRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Tj.Livraria.Domain.Entities;
+using Tj.Livraria.Domain.Interfaces.Repository;
+
+namespace Tj.Livraria.Infra.Repositories
+{
+    public class AssuntoRepository : IAssuntoRepository
+    {
+        private string ConnectionString = "Data Source=localhost;Initial Catalog=Livraria;Trusted_Connection=True;";
+
+        public void Add(Assunto entity)
+        {
+            string query = "Insert into Assunto (Descricao) values (@Descricao)";
+
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                conn.Execute(query, new
+                {
+                    entity.Descricao
+                });
+            }
+        }
+
+        public void Delete(int cod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assunto Get(int cod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Assunto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Assunto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tj.Livraria/Tj.Livraria.Tests/Infra/Repositories/AssuntoRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using System;
+using Tj.Livraria.Domain.Entities;
+using Tj.Livraria.Domain.Interfaces.Repository;
+using Tj.Livraria.Infra.Repositories;
+using Xunit;
+
+namespace Tj.Livraria.Tests.Infra.Repositories
+{
+    public class AssuntoRepositoryTests
+    {
+        [Trait("AssuntoRepository", "When Adding"), Fact(DisplayName = "Valid Arguments")]
+        public void AddingValidArguments()
+        {
+            // Arrange
+            Assunto assunto = new Assunto
+            {
+                Descricao = "Terror"
+            };
+
+            IAssuntoRepository repository = new AssuntoRepository();
+
+            // Act & Assert
+            try
+            {
+                repository.Add(assunto);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Add not working correctly", ex);
+            }
+        }
+
+        [Trait("AssuntoRepository", "When Adding"), Fact(DisplayName = "Invalid Arguments")]
+        public void AddingInvalidArguments()
+        {
+
+        }
+    }
+}
