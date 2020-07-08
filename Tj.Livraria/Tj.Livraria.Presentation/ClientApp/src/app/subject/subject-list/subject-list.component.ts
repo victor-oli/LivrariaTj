84282968b9e753e03976a01083c754d2a458ca9c
Victor Oliveira <jvespinosa@stone.com.br>
2020-07-08 17:04:39 -0300

    idem

--- Tj.Livraria/Tj.Livraria.Presentation/ClientApp/src/app/subject/subject-list/subject-list.component.ts
+++ Tj.Livraria/Tj.Livraria.Presentation/ClientApp/src/app/subject/subject-list/subject-list.component.ts
@@ -20,17 +20,7 @@ export class SubjectListComponent implements OnInit {
       );
   }
 
-  edit(cod: number) {
-    console.log(cod);
-  }
-
-  delete(cod: number) {
-    console.log(cod);
-  }
-
   onRefresh() {
-    console.log('onRefresh foi chamado!');
-
     this.service.getSubjects()
       .subscribe(
         subjectListResult => this.subjectList = subjectListResult,
