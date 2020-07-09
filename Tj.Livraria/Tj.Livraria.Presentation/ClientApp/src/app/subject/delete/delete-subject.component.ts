6c9dfb97817749566c6d0b8bbd3ebeeff40fd2f2
Victor Oliveira <jvespinosa@stone.com.br>
2020-07-09 16:00:33 -0300

    add delete subject component

--- Tj.Livraria/Tj.Livraria.Presentation/ClientApp/src/app/subject/delete/delete-subject.component.ts
+++ Tj.Livraria/Tj.Livraria.Presentation/ClientApp/src/app/subject/delete/delete-subject.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input, TemplateRef, Output, EventEmitter } from "@angular/core";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { SubjectService } from "../subject.service";
 
 @Component({
   selector: 'delete-subject',
@@ -6,5 +8,44 @@ import { Component } from "@angular/core";
 })
 
 export class DeleteSubjectComponent {
+  @Input() public subjectCod: number;
+  @Input() public subjectDescription: string;
+  @Output() onCantDeleteEvent = new EventEmitter<any>();
+  @Output() onCloseEvent = new EventEmitter<any>();
 
+  private modalRef: BsModalRef;
+
+  constructor(private service: SubjectService, private modalService: BsModalService) { }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-ls' });
+  }
+
+  confirm() {
+    this.service
+      .deleteSubject(this.subjectCod)
+      .subscribe(
+        result => {
+          if (!result.success)
+            this.onCantDeleteEvent.emit({
+              message: result.message,
+              type: 'danger'
+            });
+          else {
+            this.onCloseEvent.emit();
+            this.onCantDeleteEvent.emit({
+              message: this.subjectDescription + ' has been deleted',
+              type: 'success'
+            });
+          }
+
+          this.modalRef.hide();
+        },
+        error => console.error(error)
+      );
+  }
+
+  decline(): void {
+    this.modalRef.hide();
+  }
 }
