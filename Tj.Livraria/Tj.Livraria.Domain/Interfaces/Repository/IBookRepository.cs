7f543128b88ae3c4a2059c92ebe5f7fe4cc7905c
Victor Oliveira <jvespinosa@stone.com.br>
2020-07-09 17:58:09 -0300

    new query contract

--- Tj.Livraria/Tj.Livraria.Domain/Interfaces/Repository/IBookRepository.cs
+++ Tj.Livraria/Tj.Livraria.Domain/Interfaces/Repository/IBookRepository.cs
@@ -6,5 +6,6 @@ namespace Tj.Livraria.Domain.Interfaces.Repository
     public interface IBookRepository : IRepository<Book>
     {
         List<Book> GetAllBySubject(int subjectCod);
+        List<Book> GetAllByAuthor(int authorCod);
     }
 }
