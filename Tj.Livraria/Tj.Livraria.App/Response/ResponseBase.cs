817e3a3510e6c389fc358dbc6a2bb38719d02909
Victor Oliveira <jvespinosa@stone.com.br>
2020-07-09 17:56:48 -0300

    rename class

--- Tj.Livraria/Tj.Livraria.App/Response/ResponseBase.cs
+++ Tj.Livraria/Tj.Livraria.App/Response/ResponseBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tj.Livraria.App.Response
 {
-    public class BaseResponse
+    public class ResponseBase
     {
         public bool Success { get; set; }
         public string Message { get; set; }
