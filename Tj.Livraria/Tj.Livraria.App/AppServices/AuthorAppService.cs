b29d93a3588863a3a553b3cf28ae80797a9fff86
Victor Oliveira <jvespinosa@stone.com.br>
2020-07-09 17:21:40 -0300

    implement author app service

---
+++ Tj.Livraria/Tj.Livraria.App/AppServices/AuthorAppService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Tj.Livraria.App.Interfacies;
+using Tj.Livraria.Domain.Entities;
+using Tj.Livraria.Domain.Interfaces.Service;
+
+namespace Tj.Livraria.App.AppServices
+{
+    public class AuthorAppService : IAuthorAppService
+    {
+        private IAuthorService _service;
+
+        public AuthorAppService(IAuthorService service)
+        {
+            _service = service;
+        }
+
+        public bool Add(Author entity)
+        {
+            return _service.Add(entity);
+        }
+
+        public bool Delete(int cod)
+        {
+            return _service.Delete(cod);
+        }
+
+        public Author Get(int cod)
+        {
+            return _service.Get(cod);
+        }
+
+        public List<Author> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        public bool Update(Author entity)
+        {
+            return _service.Update(entity);
+        }
+    }
+}
---
+++ Tj.Livraria/Tj.Livraria.App/Interfacies/IAuthorAppService.cs
@@ -0,0 +1,8 @@
+﻿using Tj.Livraria.Domain.Entities;
+
+namespace Tj.Livraria.App.Interfacies
+{
+    public interface IAuthorAppService : IAppService<Author>
+    {
+    }
+}
