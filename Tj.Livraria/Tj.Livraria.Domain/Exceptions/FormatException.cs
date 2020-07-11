f4e946674dd72faf62c085209a03356ae735afcb
Victor Oliveira <jvespinosa@stone.com.br>
2020-07-11 15:30:04 -0300

    fix parent

--- Tj.Livraria/Tj.Livraria.Domain/Exceptions/FormatException.cs
+++ Tj.Livraria/Tj.Livraria.Domain/Exceptions/FormatException.cs
@@ -1,8 +1,6 @@
-﻿using System;
-
-namespace Tj.Livraria.Domain.Exceptions
+﻿namespace Tj.Livraria.Domain.Exceptions
 {
-    public class FormatException : Exception
+    public class FormatException : LibraryException
     {
         public FormatException(string message) : base(message) { }
     }
